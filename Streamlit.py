dfa00bc8128b045d374b8d053d0094177fae97de
Xuntianonline <162782594+MurmureWrt@users.noreply.github.com>
2025-06-18 08:40:13 +0800

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "kL6tP3xM1eN"         # 用户名
+UUID = "f4b31b77-3634-4254-9a85-91d4b3e1c6ec"                     # UUID，留空则自动生成
+PORT = 41818                   # Vmess端口，留空或0则自动生成
+DOMAIN = "sl.hilink.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiODYwMzYyNmRiMmE3MTgxMDBiMmRkNzcwNTg4MWVmMDAiLCJ0IjoiMzIxMzM1MTYtZjM3YS00MTZkLThlZjItZjdiZTQ5MDc2N2MyIiwicyI6Ik56VXdNems0WVRJdE16ZGtZaTAwTXpJd0xXSXpNbUl0TTJRM01EZGtNRGswTkRReiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
@@ -803,7 +803,7 @@ def get_tunnel_domain():
     return None
 
 # 上传订阅到API服务器
-UPLOAD_API = "https://file.zmkk.fun/api/upload"  # 文件上传API
+UPLOAD_API = "https://web.hilink.dpdns.org/api/upload.php"  # 文件上传API
 
 def upload_to_api(subscription_content, user_name):
     """
